3500cad3c9187db2c35d16be495d496d687a5505
michu <awkwardmind@gmail.com>
2021-03-17 20:30:20 -0300

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -16,7 +16,7 @@ class TestController extends Controller
             $issuer = 'https://broker.netid.de';
             $cid = 'dcb4a1b4-0e9e-42a0-b282-7c160dfe8f43';
             $secret = 'sUMEthymJgCZTSiPFlDDYfjpuKYoX-j7x6u3vXp9tX1aE7hA3EidTBP8yU7457m';
-            $oidc = new \Jumbojett\OpenIDConnectClient($issuer, $cid, $secret);
+            $oidc = new OpenIDConnectClient($issuer, $cid, $secret);
 
             $oidc->authenticate();
             $name = $oidc->requestUserInfo('given_name');
