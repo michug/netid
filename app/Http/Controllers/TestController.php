d0df97f62b0a0fc66f1d4b00615a042e3a8977f5
michu <awkwardmind@gmail.com>
2021-03-18 17:34:45 -0300

    asdsad

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -21,6 +21,11 @@ class TestController extends Controller
             $secret = 'sUMEthymJgCZTSiPFlDDYfjpuKYoX-j7x6u3vXp9tX1aE7hA3EidTBP8yU7457m';
             $oidc = new OpenIDConnectClient($issuer, $cid, $secret);
 
+            if (App::environment('local')) {
+               $oidc->setVerifyHost(false);
+               $oidc->setVerifyPeer(false);
+            }
+
             // default scope is "openid"
             $oidc->addScope(['profile']);
             $oidc->addAuthParam(array('claims'=>'{"userinfo":{"birthdate":{"essential":true},"gender":{"essential":true},"given_name":{"essential":true},"family_name":{"essential":true}}}'));
