65832a2c08f2b7c80a208d12686930c60c7457a1
michu <awkwardmind@gmail.com>
2021-03-17 21:42:13 -0300

    sdasd

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -17,7 +17,6 @@ class TestController extends Controller
             $cid = 'dcb4a1b4-0e9e-42a0-b282-7c160dfe8f43';
             $secret = 'sUMEthymJgCZTSiPFlDDYfjpuKYoX-j7x6u3vXp9tX1aE7hA3EidTBP8yU7457m';
             $oidc = new OpenIDConnectClient($issuer, $cid, $secret);
-            $oidc->unsetNonce();
             $oidc->addScope('openid profile email phone');
             // $oidc->addScope(['email', 'given_name']);
 
