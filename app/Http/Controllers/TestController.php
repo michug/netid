5c512482f34fc8c83508fdacf986070b61e1d228
michu <awkwardmind@gmail.com>
2021-03-18 10:03:10 -0300

    asd

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -22,9 +22,7 @@ class TestController extends Controller
             $oidc = new OpenIDConnectClient($issuer, $cid, $secret);
 
             // default scope is "openid"
-            $oidc->addScope('openid');
-            $oidc->addScope('profile');
-            $oidc->addScope('family_name');
+            $oidc->addScope(['openid', 'profile', 'family_name']);
 
             $this->printScopes($oidc); // DEBUG
 
