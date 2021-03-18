090bf087a7e5cca419c32c2d8621b345128c19c1
michu <awkwardmind@gmail.com>
2021-03-18 08:47:05 -0300

    asd

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use \Log;
 use Illuminate\Http\Request;
+
 use \Jumbojett\OpenIDConnectClient;
+
+use App\Http\Controllers\Controller;
 
 class TestController extends Controller
 {
@@ -17,27 +20,57 @@ class TestController extends Controller
             $cid = 'dcb4a1b4-0e9e-42a0-b282-7c160dfe8f43';
             $secret = 'sUMEthymJgCZTSiPFlDDYfjpuKYoX-j7x6u3vXp9tX1aE7hA3EidTBP8yU7457m';
             $oidc = new OpenIDConnectClient($issuer, $cid, $secret);
-            $oidc->addScope('openid profile email phone');
-            // $oidc->addScope(['email', 'given_name']);
 
-            $scopes = $oidc->getScopes();
-            foreach ($scopes as $scope) {
-                \Log::info('scope: ' . $scope);
-            }
+            // default scope is "openid"
+            $oidc->addScope('openid profile email given_name');
+
+            $this->printScopes($oidc); // DEBUG
 
             $oidc->authenticate();
+
             $name = $oidc->requestUserInfo('given_name');
+
+            $email = $oidc->requestUserInfo('email');
+
+            // $customer = $this->getCustomer($email);
+            // login $customer and redirect apropiatelly
+
+
             $sub = $oidc->requestUserInfo('sub');
+            \Log::info('requested UserInfo sub: ' . $value);
 
-            foreach ($oidc->getVerifiedClaims() as $key => $value) {
-                \Log::info($key . ' ' . $value);
-            }
-
-            \Log::info('sub ' . $sub);
-            \Log::info('name ' . $name);
+            $this->printVerifiedClaims($oidc);
 
         } catch (\Exception $ex) {
             \Log::info($ex->getMessage());
+        }
+    }
+
+
+    private function getCustomer($email)
+    {
+        $customer = Customer::findOrFail('email', $email)->first();
+
+        if (!$customer) {
+            // $customer = register new customer
+        }
+
+        return $customer;
+    }
+
+    private function printVerifiedClaims($oidc)
+    {
+        foreach ($oidc->getVerifiedClaims() as $key => $value) {
+            \Log::info($key . ': ' . $value);
+        }
+    }
+
+
+    private function printScopes($oidc)
+    {
+        $scopes = $oidc->getScopes();
+        foreach ($scopes as $scope) {
+            \Log::info('scope: ' . $scope);
         }
     }
 }
